1da8a2be274d73b2c4ff2eaedab1b37b5c34b5ae
David L. Qiu <david@qiu.dev>
2026-02-04 13:34:49 -0800

    update test_routes module to contain at least 1 test

--- jupyter_ai_acp_client/tests/test_routes.py
+++ jupyter_ai_acp_client/tests/test_routes.py
@@ -1,17 +1,13 @@
 # import json
 
+from tornado.httpclient import HTTPClientError
 
-# async def test_hello(jp_fetch):
-#     # When
-#     response = await jp_fetch("jupyter-ai-acp-client", "hello")
-
-#     # Then
-#     assert response.code == 200
-#     payload = json.loads(response.body)
-#     assert payload == {
-#             "data": (
-#                 "Hello, world!"
-#                 " This is the '/jupyter-ai-acp-client/hello' endpoint."
-#                 " Try visiting me in your browser!"
-#             ),
-#         }
+async def test_slash_commands_route_no_chat(jp_fetch):
+    """
+    Expects that the /ai/acp/slash_commands route returns a 400 when no ?chat_id
+    URL query argument is given.
+    """
+    try:
+        await jp_fetch("ai", "acp", "slash_commands")
+    except HTTPClientError as e:
+        assert e.code == 400
